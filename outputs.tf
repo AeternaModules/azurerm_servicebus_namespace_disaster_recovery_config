4109f9a480de208646d4970e2b4adb36e3332937
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:50 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,41 @@
 output "servicebus_namespace_disaster_recovery_configs_id" {
   description = "Map of id values across all servicebus_namespace_disaster_recovery_configs, keyed the same as var.servicebus_namespace_disaster_recovery_configs"
-  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.id }
+  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "servicebus_namespace_disaster_recovery_configs_alias_authorization_rule_id" {
   description = "Map of alias_authorization_rule_id values across all servicebus_namespace_disaster_recovery_configs, keyed the same as var.servicebus_namespace_disaster_recovery_configs"
-  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.alias_authorization_rule_id }
+  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.alias_authorization_rule_id if v.alias_authorization_rule_id != null && length(v.alias_authorization_rule_id) > 0 }
 }
 output "servicebus_namespace_disaster_recovery_configs_default_primary_key" {
   description = "Map of default_primary_key values across all servicebus_namespace_disaster_recovery_configs, keyed the same as var.servicebus_namespace_disaster_recovery_configs"
-  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.default_primary_key }
+  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.default_primary_key if v.default_primary_key != null && length(v.default_primary_key) > 0 }
   sensitive   = true
 }
 output "servicebus_namespace_disaster_recovery_configs_default_secondary_key" {
   description = "Map of default_secondary_key values across all servicebus_namespace_disaster_recovery_configs, keyed the same as var.servicebus_namespace_disaster_recovery_configs"
-  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.default_secondary_key }
+  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.default_secondary_key if v.default_secondary_key != null && length(v.default_secondary_key) > 0 }
   sensitive   = true
 }
 output "servicebus_namespace_disaster_recovery_configs_name" {
   description = "Map of name values across all servicebus_namespace_disaster_recovery_configs, keyed the same as var.servicebus_namespace_disaster_recovery_configs"
-  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.name }
+  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "servicebus_namespace_disaster_recovery_configs_partner_namespace_id" {
   description = "Map of partner_namespace_id values across all servicebus_namespace_disaster_recovery_configs, keyed the same as var.servicebus_namespace_disaster_recovery_configs"
-  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.partner_namespace_id }
+  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.partner_namespace_id if v.partner_namespace_id != null && length(v.partner_namespace_id) > 0 }
 }
 output "servicebus_namespace_disaster_recovery_configs_primary_connection_string_alias" {
   description = "Map of primary_connection_string_alias values across all servicebus_namespace_disaster_recovery_configs, keyed the same as var.servicebus_namespace_disaster_recovery_configs"
-  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.primary_connection_string_alias }
+  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.primary_connection_string_alias if v.primary_connection_string_alias != null && length(v.primary_connection_string_alias) > 0 }
   sensitive   = true
 }
 output "servicebus_namespace_disaster_recovery_configs_primary_namespace_id" {
   description = "Map of primary_namespace_id values across all servicebus_namespace_disaster_recovery_configs, keyed the same as var.servicebus_namespace_disaster_recovery_configs"
-  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.primary_namespace_id }
+  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.primary_namespace_id if v.primary_namespace_id != null && length(v.primary_namespace_id) > 0 }
 }
 output "servicebus_namespace_disaster_recovery_configs_secondary_connection_string_alias" {
   description = "Map of secondary_connection_string_alias values across all servicebus_namespace_disaster_recovery_configs, keyed the same as var.servicebus_namespace_disaster_recovery_configs"
-  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.secondary_connection_string_alias }
+  value       = { for k, v in azurerm_servicebus_namespace_disaster_recovery_config.servicebus_namespace_disaster_recovery_configs : k => v.secondary_connection_string_alias if v.secondary_connection_string_alias != null && length(v.secondary_connection_string_alias) > 0 }
   sensitive   = true
 }
 
